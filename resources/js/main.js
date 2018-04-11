006b398d4bddf63560c2a3f76cb2b4615382568a
Zara Nunn <ZaraNunn@Zaras-iMac.local>
2018-04-11 12:58:33 +0100

    Carousel added.

---
+++ resources/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+
+
+  $('.carousel').slick({
+    slidestoShow: 1,
+    slidestoScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseonFocus: false,
+    pauseonHover: false,
+    infinite: true,
+    adaptiveHeight: true,
+  });
+
+});
