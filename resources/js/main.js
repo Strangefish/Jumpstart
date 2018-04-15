9be02cd696ad791777ede0526826e2f7f55e01f0
Zara Nunn <ZaraNunn@Zaras-iMac.local>
2018-04-15 13:49:54 +0100

    Final commit. Had to look at solution for points 24 - 32.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,6 @@
 
+
+
 $(document).ready(function() {
 
 
@@ -14,14 +16,23 @@ $(document).ready(function() {
     pauseonHover: false,
     infinite: true,
     adaptiveHeight: true,
-  });
-
-  $('.navbar').animate({top: '-' + z}, 150);
-
 });
 
-var scrollDistance = 0
 
-.scroll('body')
+var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+    var navHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + navHeight}, 150);
+    lastScrollTop = scrollTop;
+  } else if (lastScrollTop - scrollTop > 50) {
+    $('.navbar').animate({top: '0px'}, 150);
+    lastScrollTop = scrollTop;
+  }
+});
 
-var y = $(this).scrollTop();
+
+
+
+});
